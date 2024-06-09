34942d7d71e7897560c29213d8b7f17e6f394fa7
hieuzeta <hieu@zetamotion.com>
2024-06-09 20:36:28 +0700

    NKL

--- todo-app-ui/src/App.tsx
+++ todo-app-ui/src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [NKL, setNKL] = useState(false);
+
+  useEffect(() => {
+    if (!NKL) setNKL(true);
+  }, [NKL]);
 
   return (
     <>
+      {NKL && "ADS"}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
